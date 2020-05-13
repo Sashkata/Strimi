a80a22c355469a8c5239613c823738f7295de739
Sashkata <alekspenev347@gmail.com>
2020-05-13 12:19:20 -0500

    added port

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -30,7 +30,7 @@ class StreamShow extends React.Component {
 
     this.player = flv.createPlayer({
       type: "flv",
-      url: `https://73.22.64.107/live/${this.props.match.params.id}.flv`,
+      url: `https://73.22.64.107:25546/live/${this.props.match.params.id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
